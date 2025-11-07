9cf43316b1cdb1e9a2b3783fa1f6ee9753da79b4
Hiroyuki <lahm0107@gmail.com>
2025-11-08 08:35:47 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by hiroyuki takahashi on R 7/11/08.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
